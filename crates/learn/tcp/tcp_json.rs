ac2df34e9027545bfe67d9992f8effacea33b8d6
YongHyun Park <67228941+enkeejunior1@users.noreply.github.com>
2023-07-04 21:48:09 +0900

    tcp json에 간단한 구조를 넣는 작업 중

--- crates/learn/tcp/tcp_json.rs
+++ crates/learn/tcp/tcp_json.rs
@@ -1,17 +1,21 @@
 use dashmap::DashMap;
-use tokio::net::TcpStream;
-use std::sync::Arc;
-use std::sync::RwLock;
-
+use tokio::net::{ TcpListener, TcpStream };
+use tokio::io::{ AsyncReadExt, AsyncWriteExt };
+use bytes::BytesMut;
+use std::net::SocketAddr;
+use std::sync::{Arc, RwLock};
 use anyhow::Error;
 
 struct Server {
     addr: String,
-    sessions: Arc<DashMap<u32, Session>>,
+    sessions: Arc<DashMap<String, Arc<RwLock<Session>>>>,
 }
 
 struct Session {
-    stream: TcpStream
+    sessions: Arc<DashMap<String, Arc<RwLock<Session>>>>,
+    stream: TcpStream,
+    addr: SocketAddr,
+    buffer: BytesMut,
 }
 
 struct Client {
@@ -19,7 +23,6 @@ struct Client {
 }
 
 impl Server {
-
     pub fn new(addr: String) -> Self {
         Self {
             addr,
@@ -27,22 +30,75 @@ impl Server {
         }
     }
 
+    #[allow(unreachable_code)] // XXX: temporal
     pub async fn run(&mut self) -> Result<(), Error> {
+        let server = TcpListener::bind(&self.addr).await?;
 
-        // bind, listen
-        // accept loop
+        loop {
+            // 동작하는 구조를 만들고 완전하게 한다. 헤맬 수 밖에 없다.
+            // 찬찬히 다시 생각해야겠다. 어디서 어떻게 해야 할까?
+            // 서버에 접근할 필요가 있다. 싱글톤처럼.
+
+            let stream = server.accept().await?;
+            let sessions = self.sessions.clone();
+            let session = Arc::new(RwLock::new(Session::new(sessions, stream.0, stream.1)));
+            let working_session = session.clone();
+
+            self.sessions.insert(session.addr.to_string(), session);
+
+            tokio::spawn(async move {
+                let result = working_session.run().await;
+                if let Err(e) = result {
+                    println!("{}", e);
+                }
+            });
+
+            // oneshot channel try_recv()로 종료 확인?
+        }
 
         Ok(())
     }
 }
 
 impl Session {
-    // new 
-    // run
+    // new
+    fn new(
+        sessions: Arc<DashMap<String, Arc<RwLock<Session>>>>,
+        stream: TcpStream,
+        addr: SocketAddr
+    ) -> Self {
+        Self {
+            sessions,
+            stream,
+            addr,
+            buffer: BytesMut::with_capacity(4096),
+        }
+    }
+
+    #[allow(unreachable_code)]
+    async fn run(&mut self) -> Result<(), Error> {
+        loop {
+            let result = self.stream.read_buf(&mut self.buffer).await;
+
+            match result {
+                Err(e) => {
+                    // error
+                }
+                Ok(len) => {
+                    if len == 0 {
+                        // peer disconnected
+                    } else {
+                        // process the buffer
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
 
-impl Client { 
-
+impl Client {
     pub fn new(addr: String) -> Self {
         Self {
             addr,
@@ -60,11 +116,10 @@ mod tests {
 
     #[tokio::test]
     async fn test_echo() {
-
-        let mut server = Server::new("127.0.0.1:7000".to_string()); 
+        let mut server = Server::new("127.0.0.1:7000".to_string());
 
         // server.run()
-        // use tokio::spawn 또는 JoinHandle로 대기 
+        // use tokio::spawn 또는 JoinHandle로 대기
 
         let mut client = Client::new("127.0.0.1:7000".to_string());
         // client.run()
