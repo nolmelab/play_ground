4bdf6cfee77f4d58dba0a4c121f269a962b39659
nolmelab <nolmelab@gmail.com>
2024-05-17 09:26:16 +0900

    authentication & encryption

--- exercise/code/sodium/ex_sodium.cpp
+++ exercise/code/sodium/ex_sodium.cpp
@@ -70,6 +70,23 @@ TEST_CASE("libsodium")
     // https://doc.libsodium.org/secret-key_cryptography/secretbox
     // 
 
+    #define MESSAGE ((const unsigned char *) "test")
+    #define MESSAGE_LEN 4
+    #define CIPHERTEXT_LEN (crypto_secretbox_MACBYTES + MESSAGE_LEN)
+
+    unsigned char key[crypto_secretbox_KEYBYTES];
+    unsigned char nonce[crypto_secretbox_NONCEBYTES];
+    unsigned char ciphertext[CIPHERTEXT_LEN];
+
+    crypto_secretbox_keygen(key);
+    randombytes_buf(nonce, sizeof nonce);
+    crypto_secretbox_easy(ciphertext, MESSAGE, MESSAGE_LEN, nonce, key);
+
+    unsigned char decrypted[MESSAGE_LEN];
+    if (crypto_secretbox_open_easy(decrypted, ciphertext, CIPHERTEXT_LEN, nonce, key) != 0) {
+      /* message forged! */
+    }
+
   }
 
   SUBCASE("key exchange")
